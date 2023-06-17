74b04df3bd1b14d1df457b131ef3a3d722a5c3ed
Daniela PB <danielapatinob98@gmail.com>
2023-06-17 21:52:21 +0200

    Adds: CreateTodoButton

---
+++ src/CreateTodoButton/index.js
@@ -0,0 +1,10 @@
+import './CreateTodoButton.css'
+
+function CreateTodoButton() {
+  return(
+    <button className="createTodoButton" onClick={() => {console.log('holi');}}>+</button>
+  )
+
+}
+
+export { CreateTodoButton };
