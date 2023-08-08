799e317b835813549bd1d8fe72ff42e96ed15792
Daniela PB <danielapatinob98@gmail.com>
2023-08-08 21:19:40 +0200

    Fixes: linter warnings

--- src/App/index.js
+++ src/App/index.js
@@ -2,15 +2,20 @@ import React from 'react';
 import { TodoProvider } from '../TodoContext';
 import { AppUI } from './AppUI';
 
-const defaultTodos = [
-  { text: 'Cortar cebolla', completed: true},
-  { text: 'Tomar Curso', completed: false},
-  { text: 'Meditar', completed: true},
-  { text: 'Escribir', completed: false},
-  { text: 'Correr', completed: false},
-]
+// #### for testing purposes  ####
+
+// const defaultTodos = [
+//   { text: 'Cortar cebolla', completed: true},
+//   { text: 'Tomar Curso', completed: false},
+//   { text: 'Meditar', completed: true},
+//   { text: 'Escribir', completed: false},
+//   { text: 'Correr', completed: false},
+// ]
 
 // localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos))
+
+// ################################
+
 function App() {
   return (
     <TodoProvider>
